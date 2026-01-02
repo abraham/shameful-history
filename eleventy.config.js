9fa181bac94e7dba3e958b0470dc1abfcf0bdb45
Abraham Williams <4braham@gmail.com>
2026-01-01 19:45:49 -0600

    Split up styling

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addFilter("readableDate", (dateObj) => {
         return dateObj.toLocaleDateString('en-US', {
             year: 'numeric',

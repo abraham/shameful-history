a884be9c724d2ecc7afa6751e0765b364a71723e
Abraham Williams <4braham@gmail.com>
2026-01-01 23:27:53 -0600

    Add image plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,32 @@
+import Image from "@11ty/eleventy-img";
+
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("img");
+
+    eleventyConfig.addAsyncShortcode("image", async function (src, alt, sizes = "100vw") {
+        if (alt === undefined) {
+            // You bet we throw an error on missing alt (alt="" works okay)
+            throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+        }
+
+        let metadata = await Image(src, {
+            widths: [300, 600, "auto"],
+            formats: ["avif", "webp", "jpeg", "svg"],
+            outputDir: "./_site/img/",
+        });
+
+        let imageAttributes = {
+            alt,
+            sizes,
+            loading: "lazy",
+            decoding: "async",
+        };
+
+        return Image.generateHTML(metadata, imageAttributes);
+    });
+
     eleventyConfig.addFilter("readableDate", (dateObj, endDate) => {
         const options = {
             year: 'numeric',
